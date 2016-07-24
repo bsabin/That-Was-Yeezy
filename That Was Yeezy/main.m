0c151741d7bc3326f5909c01a11f15552d274924
Barry Sabin <barrysabin@barrys-macbook-pro.local>
2016-07-24 14:30:33 -0400

    Initial Commit

---
+++ That Was Yeezy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  That Was Yeezy
+//
+//  Created by Barry Sabin on 7/24/16.
+//  Copyright © 2016 Barry Sabin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
